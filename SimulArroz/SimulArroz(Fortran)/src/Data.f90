eaedc7b153ad5061ac9f348d4e5ffbacf49a7f2f
Giovana1 <giovanagribas1@gmail.com>
2016-06-04 17:38:18 -0300

    * continuei declarando as variaveis no data.f90

--- SimulArroz/SimulArroz(Fortran)/src/Data.f90
+++ SimulArroz/SimulArroz(Fortran)/src/Data.f90
@@ -7,27 +7,37 @@ module SEED
     integer*2 initYear, lastYear, initDay ; integer*4 i, ioErr 
     
     integer*4 CO2, resposta1, grupoMaturacao, cultivar,  populacao, &
-                resposta3
-
-    logical*2 simulating, firstDay
+                resposta3,  TBRUE, TO1RUE, TO2RUE, TMRUE,  DAYLC, AN
+    character nome*14
+    logical*2 simulating, firstDay, vegetative, reprodutive, grainfill
     
-    real*4 HS(nn), DVS(nn), LARMAX(nn), NFF(nn), LAI(nn), GCROP(nn)
+    real*4 HS(nn), DVS(nn), LARMAX(nn), NFF(nn), LAI(nn), GCROP(nn), & 
+            RUE(nn), PAR(nn), KDF(nn), PSTPAR(nn), fNT, DRLV(nn), DVSPI, DVSF
     
     real*4 STVG(nn),STRP(nn),STEG(nn)
     
-    real*4 TTEM, TTVG, TTRP, TTEG, LARMAX
-    real*4 PMAX, E, FC1, alfa, FT,
+    real*4 TTEM, TTVG, TTRP, TTEG, TCMIN, TCOT, TCMAX 
+    real*4 PMAX, E, FC1, FC, alfa, FT,
     
-    real*4 DREM(nn), DRVG(nn), DRRP(nn), DREG(nn)
+    real*4 DREM(nn), DRVG(nn), DRRP(nn), DREG(nn), NCOLD(nn), GEADA, &
+            DAS(nn), MAXSTD (nn), 
     
-    real*4 RWLVG(nn), RWSTG(nn), RWSOG(nn)
+    real*4  RWLVG(nn), RWSTG(nn), DST(nn), GST(nn)
+    real*4  RWSOG(nn), WSOG(nn), RESERVA
+    real*4  FSH, FLV, FST, FSO, DLV(nn), DRLV(nn), WLVG(nn), &
+            WSTG(nn), WRTG(nn), RWTRG(nn)
     
-    real*4 WLVG(nn), WSTG(nn), WSOG(nn)
+    real*4 SLA(nn), ASLA, SSGA, BSLA, CSLA, DSLA, RLAI(nn),GLAI(nn), & 
+            DLA(nn), LALOSS,NSLLV
     
-    real*4 SODAY(nn), TOTALSO(nn), TOTALG(nn), PARCIAL(nn)
+    real*4 CTT(nn), COLDTT(nn)
+    real*4 TFERT(nn), NTFERT(nn), TMM(nn)
     
-    real*4 PESODIA(nn), PESO1(nn), RENDkghaMS(nn), RENDkgha13(nn), &
-            RENDscha13(nn),  SFCOLD(nn), SFHEAT(nn)
+    real*4 SOCF, SODAY(nn), TOTALSO(nn), TOTALG(nn), PARCIAL(nn), & 
+            STRILE(nn), SOFILL(nn) 
+    
+    real*4 PESODIA(nn), PESO1(nn), PESOG(nn), RENDkghaMS(nn), & 
+            RENDkgha13(nn), RENDscha13(nn),  SFCOLD(nn), SFHEAT(nn), & 
+            SPFERT, TOTALG, 
             
-    real*4 fNT
 end module
